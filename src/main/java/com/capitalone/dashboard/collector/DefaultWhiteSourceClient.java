6da0bf89781ea85797ab16e20c043b8c3d16e86b
nireeshT <nireesh.thiruveedula@capitalone.com>
2021-03-16 13:37:44 -0400

    RequestLog - added source

--- src/main/java/com/capitalone/dashboard/collector/DefaultWhiteSourceClient.java
+++ src/main/java/com/capitalone/dashboard/collector/DefaultWhiteSourceClient.java
@@ -25,6 +25,7 @@ import com.capitalone.dashboard.settings.WhiteSourceServerSettings;
 import com.capitalone.dashboard.settings.WhiteSourceSettings;
 import com.capitalone.dashboard.utils.Constants;
 import com.capitalone.dashboard.utils.DateTimeUtils;
+import com.mongodb.util.JSON;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -796,4 +797,8 @@ public class DefaultWhiteSourceClient implements WhiteSourceClient {
         return Constants.NONE;
     }
 
+
+    public static void main(String args[]){
+        JSON.parse("{hello whitesource}");
+    }
 }
