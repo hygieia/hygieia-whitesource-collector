17421b5d24c67258a6248d7d1b434bf8379ad31e
nireeshT <nireesh.thiruveedula@capitalone.com>
2021-03-16 14:30:46 -0400

    Added application to RequestLog

--- src/main/java/com/capitalone/dashboard/logging/LoggingFilter.java
+++ src/main/java/com/capitalone/dashboard/logging/LoggingFilter.java
@@ -62,6 +62,8 @@ public class LoggingFilter implements Filter {
 
     private static final String PING = "ping";
 
+    private static final String WHITESOURCE = "WhiteSource";
+
     @Autowired
     private RequestLogRepository requestLogRepository;
 
@@ -100,6 +102,7 @@ public class LoggingFilter implements Filter {
         requestLog.setMethod(httpServletRequest.getMethod());
         requestLog.setParameter(requestMap.toString());
         requestLog.setApiUser(apiUser);
+        requestLog.setApplication(WHITESOURCE);
         if(StringUtils.isNotEmpty(correlation_id)) {
             requestLog.setClientReference(correlation_id);
         }
@@ -116,6 +119,7 @@ public class LoggingFilter implements Filter {
 
             LOGGER.info("correlation_id=" + correlation_id
                     + ", requester=" + apiUser
+                    + ", source=" + WHITESOURCE
                     + ", duration=" + (System.currentTimeMillis() - startTime)
                     + ", application=hygieia, service=whitesource-collector"
                     + ", uri=" + bufferedRequest.getRequestURI()
@@ -150,6 +154,7 @@ public class LoggingFilter implements Filter {
 
             LOGGER.info("correlation_id=" + correlation_id
                     + ", requester=" + apiUser
+                    + ", source=" + WHITESOURCE
                     + ", duration=" + (System.currentTimeMillis() - startTime)
                     + ", application=hygieia, service=whitesource-collector"
                     + ", uri=" + bufferedRequest.getRequestURI()
