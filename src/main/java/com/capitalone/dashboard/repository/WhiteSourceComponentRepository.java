ac3e18c2df6e4c36251ae2d591d0f38645602436
eux121 <aravindhan.murugan@capitalone.com>
2022-02-11 01:08:43 -0500

    filter by org and token

--- src/main/java/com/capitalone/dashboard/repository/WhiteSourceComponentRepository.java
+++ src/main/java/com/capitalone/dashboard/repository/WhiteSourceComponentRepository.java
@@ -1,5 +1,6 @@
 package com.capitalone.dashboard.repository;
 
+import com.capitalone.dashboard.model.CollectorItem;
 import com.capitalone.dashboard.model.WhiteSourceComponent;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.Query;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface WhiteSourceComponentRepository extends BaseCollectorItemRepository<WhiteSourceComponent> {
     @Query(value="{ 'collectorId' : ?0, enabled: true}")
     List<WhiteSourceComponent> findEnabledComponents(ObjectId collectorId);
+
+    @Query("{'options.orgName' : ?0, 'options.projectToken' : ?1}")
+    List<CollectorItem> findByOrgNameAndProjectToken(String orgName, String projectToken);
 
 }
