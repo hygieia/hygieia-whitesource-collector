f721da5a9b20ad7ab5f64b0aa98bf0e7f030ed87
dcanar9 <anthony.canar@hotmail.com>
2022-11-30 06:36:47 -0700

    fixing imports and dependencies

--- src/main/java/com/capitalone/dashboard/config/WebSecurityConfig.java
+++ src/main/java/com/capitalone/dashboard/config/WebSecurityConfig.java
@@ -1,29 +1,17 @@
 package com.capitalone.dashboard.config;
 
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.JwtPopTokenVerifier;
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.PopTokenVerifier;
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.PreAuthenticatedPopTokenHeaderProcessingFilter;
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.claims.ApiGatewayJwtClaimsValidator;
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.claims.JwtClaimsValidator;
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.rsa.ApiGatewayRsaKeyProvider;
-import com.capitalone.chassis.engine.core.security.jwt.apigateway.pop.rsa.RsaKeyProvider;
 import com.capitalone.dashboard.auth.AuthProperties;
 import com.capitalone.dashboard.auth.AuthenticationResultHandler;
-import com.capitalone.dashboard.auth.WhitesourceCollectorAuthenticationFilter;
 import com.capitalone.dashboard.auth.apitoken.ApiTokenAuthenticationProvider;
 import com.capitalone.dashboard.auth.apitoken.ApiTokenRequestFilter;
 import com.capitalone.dashboard.auth.token.JwtAuthenticationFilter;
 import com.capitalone.dashboard.auth.token.TokenAuthenticationServiceImpl;
 import com.capitalone.dashboard.client.RestClient;
-import com.capitalone.dashboard.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,14 +20,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.*;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -88,18 +73,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
-	}
-
-	@Bean
-	protected PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
-		PreAuthenticatedAuthenticationProvider preAuthProvider = new PreAuthenticatedAuthenticationProvider();
-		preAuthProvider.setPreAuthenticatedUserDetailsService(new UserDetailsByNameServiceWrapper<>(new UserDetailsService() {
-			@Override
-			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				return new User(gatewayToken, Constants.APP_NAME, Collections.singleton(new SimpleGrantedAuthority("ROLE_API")));
-			}
-		}));
-		return preAuthProvider;
 	}
 
 	@Bean
